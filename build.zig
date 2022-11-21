f5ab0d48f0047f4c009225f5e0ba4f319db9e114
Hans Musgrave <Hans.Musgrave@gmail.com>
2022-11-21 07:03:07 -0800

    try out zigmod

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
@@ -8,11 +9,13 @@ pub fn build(b: *std.build.Builder) void {
     const lib = b.addStaticLibrary("byol", "src/byol.zig");
     lib.setBuildMode(mode);
     lib.use_stage1 = true; // TODO: zig#6025
+    deps.addAllTo(lib);
     lib.install();
 
     const main_tests = b.addTest("src/byol.zig");
     main_tests.use_stage1 = true; // TODO: zig#6025
     main_tests.test_evented_io = true;
+    deps.addAllTo(main_tests);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
